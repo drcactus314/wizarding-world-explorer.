8b7564b95b29b960f79efa8954374d1537bc5c4c
Artem Kril <arturkril9@gmail.com>
2026-05-22 18:54:22 +0300

    feat: implement hero and category sections, add button event

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 console.log("hello");
+
+const button = document.querySelector('.hero-section__button');
+const category = document.querySelector('.categories-section');
+console.log(category);
+
+const getCategories =(e)=>{
+    e.preventDefault();
+    category.classList.toggle('visually-hidden');
+    category.scrollIntoView({ 
+            behavior: 'smooth', // Плавна прокрутка
+            block: 'start'})
+    console.log(category);
+    
+}
+button.addEventListener('click', getCategories);
+console.log(button);
+
